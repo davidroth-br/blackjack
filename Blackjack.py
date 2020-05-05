c760687a3bc548d5b2dd0f85e7afe23b2fc58711
David <61717416+davidroth-br@users.noreply.github.com>
2020-05-05 18:29:44 -0400

    Implemented basic dealer logic

--- Blackjack.py
+++ Blackjack.py
@@ -1,29 +1,37 @@
-from random import randint
+from random import sample, shuffle
 
-# INITIATE DECK OF CARDS
-cards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
-suits = ["\u2666", "\u2663", "\u2660", "\u2665"]
-unshuffled_deck = []
-for suit in suits:
-    for card in cards:
-        unshuffled_deck.append((card, suit))
+class Deck:
+    def __init__(self):
+        card_values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+        card_suits = ["\u2666", "\u2663", "\u2660", "\u2665"]
+        self.complete = []
+        for card_suit in card_suits:
+            for card_value in card_values:
+                self.complete.append((card_value, card_suit))
+        shuffle(self.complete)
 
-# SHUFFLE DECK
-deck = []
-while unshuffled_deck:
-    pick = randint(0, len(unshuffled_deck) - 1)
-    deck.append(unshuffled_deck.pop(pick))
+    def shuffle(self):
+        shuffle(self.complete)
+        self.shuffled = sample(self.complete, len(self.complete))
+
+    def deal_card(self):
+        return self.shuffled.pop()
 
 
 class Hand:
     def __init__(self):
         self.hand = []
+        self.totals = []
+        self.result = ""
+        self.bust = False
+        self.twenty_one = False
 
-    def add_to_hand(self):
-        self.hand.append(deal_card())
+    def add_to_hand(self, card):
+        self.hand.append(card)
 
     def sum_hand(self):
-        totals = [0]
+        self.totals = []
+        total = 0
         aces = 0
         for card in self.hand:
             if card[0] == "A":
@@ -33,16 +41,21 @@ class Hand:
                 card_value = 10
             else:
                 card_value = int(card[0])
-            totals[0] += card_value
-        for ace in range(aces):
-            totals.append(totals[0] + 1)
-            totals.append(totals[0] + 11)
-        return totals
+            total += card_value
+        if aces > 0:
+            for ace in range(aces):
+                self.totals.append(total + 1)
+                self.totals.append(total + 11)
+        else:
+            self.totals.append(total)
+        self.bust = all(total > 21 for total in self.totals)
+        self.twenty_one = any(total == 21 for total in self.totals)
+#        return totals
 
     def hand_string(self):
         hand = ""
         for card in self.hand:
-            hand += card_face(card) + " "
+            hand += card[0] + card[1] + " "
         return hand
 
     def check_result(self):
@@ -50,58 +63,124 @@ class Hand:
         busts = 0
         for total in totals:
             if total == 21:
-                return "21!!! You win!!!"
+                self.result = "21"
             if total > 21:
                 busts += 1
         if busts == len(totals):
-            return "BUST!!! You lose!!!"
-        return ""
+            self.result = "BUST"
 
 
-def y_n_input(message):
+def checked_input(message, valid_options):
     while True:
-        y_n = input(message).lower()
-        if y_n != "y" and y_n != "n":
-            print("You must choose either 'y' or 'n'.")
+        user_input = input(message).upper()
+        if user_input not in valid_options:
+            print("You must choose a valid option")
             continue
         else:
-            return y_n
-
-
-def deal_card():
-    return deck.pop()
-
-
-def card_face(card):
-    return card[0] + card[1]
+            return user_input
 
 
 print("Welcome to Blackjack!\n")
-play = y_n_input("Ready to play? (y/n): ")
-print()
-while play == "y":
+play = checked_input("Ready to play? (Y/N): ", ["Y", "N"])
+
+deck1 = Deck()
+
+while play == "Y":
+    deck1.shuffle()
     player1 = Hand()
     dealer = Hand()
 
     # FIRST DEAL
     for i in range(2):
-        player1.add_to_hand()
-    print("Your cards:")
-    print(player1.hand_string())
-    result = player1.check_result()
-    if result != "":
-        print(result)
-
+        player1.add_to_hand(deck1.deal_card())
     for i in range(2):
-        dealer.add_to_hand()
-    print("Dealer's cards:")
-    dealer_hand = dealer.hand_string()
-    print("\u2630" + dealer_hand[dealer_hand.find(" "):])
+        dealer.add_to_hand(deck1.deal_card())
 
-    while result == "":
-        break
+    keep_going = True
+    dealer_turn = False
+    end = False
+    while keep_going:
+        player1.sum_hand()
+        dealer.sum_hand()
+        keep_going = False
+
+        # SHOW HANDS
+        print("\nYour cards:")
+        print(player1.hand_string())
+        print("Dealer's cards:")
+        if not dealer_turn:
+            dealer_hand = dealer.hand_string()
+            print("\u2630" + dealer_hand[dealer_hand.find(" "):])
+        else:
+            print(dealer.hand_string())
+
+        # CHECK FOR WINNERS
+        if player1.twenty_one:
+            if dealer.twenty_one:
+                print("\nPUSH!!! No winner!!!")
+            else:
+                print("\n21!!! YOU WIN!!!")
+        elif player1.bust:
+            print("\nBUST!!! You lose!!!")
+        elif dealer.twenty_one:
+            print("\nThe dealer has 21!!! You lose!!!")
+        elif dealer.bust:
+            print("\nThe dealer busted!!! YOU WIN!!!")
+        elif end:
+            print("\nThe dealer has {0} points and you have {1} points.".format(dealer_points, player1_points))
+            if dealer_points > player1_points:
+                print("You lose!!!")
+            elif dealer_points < player1_points:
+                print("YOU WIN!!!")
+            else:
+                print("PUSH!!! No winner!!!")
+        else:
+            keep_going = True
+
+        # GAME LOGIC
+        if keep_going:
+            if not dealer_turn:
+                request = checked_input("\nH - Hit\nS - Stand\nWhat would you like to do? ", ["H", "S"])
+                if request == "H":
+                    print("\nYou hit.")
+                    player1.add_to_hand(deck1.deal_card())
+                else:
+                    print("\nYou stand.")
+                    dealer_turn = True
+            else:
+                dealer.sum_hand()
+                if len(dealer.totals) == 1:
+                    if dealer.totals[0] < 17:
+                        print("\nDealer hits.")
+                        dealer.add_to_hand(deck1.deal_card())
+                    else:
+                        print("\nDealer stands.")
+                        legal_points = [points for points in dealer.totals if points <= 21]
+                        legal_points.sort()
+                        dealer_points = legal_points[-1]
+                        legal_points = [points for points in player1.totals if points <= 21]
+                        legal_points.sort()
+                        player1_points = legal_points[-1]
+                        end = True
+                else:
+                    legal_points = [points for points in dealer.totals if points <= 21]
+                    legal_points.sort()
+                    if legal_points[-1] > 20:
+                        print("\nDealer stands.")
+                        dealer_points = legal_points[-1]
+                        legal_points = [points for points in player1.totals if points <= 21]
+                        legal_points.sort()
+                        player1_points = legal_points[-1]
+                        end = True
+                    else:
+                        print("\nDealer hits.")
+                        dealer.add_to_hand(deck1.deal_card())
 
 
 
-    play = y_n_input("\nWould you like to play again? (y/n): ")
+
+
+
+
+    play = checked_input("\nWould you like to play again? (Y/N): ", ["Y", "N"])
 print("Goodbye!")
